f60d5df2c396cdc786fa8832c1ef62e55b7ecfff
Aryan Mainali <aryanmainali03@gmail.com>
2026-03-15 15:35:31 -0500

    Reverting back

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
-  extends: ["next/core-web-vitals"],
-  plugins: ["@typescript-eslint"],
+  extends: ["next/core-web-vitals", "next/typescript"],
   rules: {
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
