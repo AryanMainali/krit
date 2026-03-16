24932ef68440a8f015916435c13e2b7d09d87156
Aryan Mainali <aryanmainali03@gmail.com>
2026-03-15 20:30:07 -0500

    Fixed duplicate revision id, unmerged head, merge migration updated

--- backend/alembic/versions/m5n6o7p8_merge_heads.py
+++ backend/alembic/versions/m5n6o7p8_merge_heads.py
@@ -1,7 +1,7 @@
-"""Merge migration heads: f6a7b8c9d0e1 and k3l4m5n6
+"""Merge migration heads: f6a7b8c9d0e2 and k3l4m5n6
 
 Revision ID: m5n6o7p8
-Revises: f6a7b8c9d0e1, k3l4m5n6
+Revises: f6a7b8c9d0e2, k3l4m5n6
 Create Date: 2026-03-15
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 
 revision: str = "m5n6o7p8"
-down_revision: Union[str, None] = ("f6a7b8c9d0e1", "k3l4m5n6")
+down_revision: Union[str, None] = ("f6a7b8c9d0e2", "k3l4m5n6")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
